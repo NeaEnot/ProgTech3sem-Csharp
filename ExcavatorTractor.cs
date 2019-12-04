57a3a423de48ba3577ff86d7aeaa12c600b971ab
NeaEnot <oleg.kozloss@yandex.ru>
2019-12-04 17:23:27 +0400

    Lab1

---
+++ ExcavatorTractor.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class ExcavatorTractor
+    {
+        private float _startPosX;
+        private float _startPosY;
+        private int _pictureWidth;
+        private int _pictureHeight;
+        private const int etWidth = 90;
+        private const int etHeight = 50;
+        public int MaxSpeed { private set; get; }
+        public float Weight { private set; get; }
+        public Color MainColor;
+        public Color DopColor;
+        public Color FlagColor;
+        public bool Flag;
+        public bool FrontTube;
+        public bool BackTube;
+
+        public Color FLAG_COLOR
+        {
+            set
+            {
+                if (value != MainColor && value != DopColor)
+                {
+                    FlagColor = value;
+                }
+                else
+                {
+                    Random rnd = new Random();
+                    Color color = Color.FromArgb(rnd.Next(255), rnd.Next(255), rnd.Next(255), rnd.Next(255));
+                    FLAG_COLOR = color;
+                }
+            }
+            get
+            {
+                return FlagColor;
+            }
+        }
+
+        public ExcavatorTractor(int maxSpeed, float weight, Color mainColor, 
+            Color dopColor, Color flagColor, bool flag, bool frontTube, bool backTube)
+        {
+            MaxSpeed = maxSpeed;
+            Weight = weight;
+            MainColor = mainColor;
+            DopColor = dopColor;
+            FLAG_COLOR = flagColor;
+            Flag = flag;
+            FrontTube = frontTube;
+            BackTube = backTube;
+        }
+
+        public void SetPosition(int x, int y, int width, int height)
+        {
+            _startPosX = x;
+            _startPosY = y;
+            _pictureWidth = width;
+            _pictureHeight = height;
+        }
+
+        public void MoveTransport(Direction direction)
+        {
+            float step = MaxSpeed * 100 / Weight;
+            switch (direction) {
+                case Direction.Right:
+                    if (_startPosX + step < _pictureWidth - etWidth) {
+                        _startPosX += step;
+                    }
+                    break;
+                case Direction.Left:
+                    if (_startPosX - step > 5) {
+                        _startPosX -= step;
+                    }
+                    break;
+                case Direction.Up:
+                    if (_startPosY - step > 0)
+                    {
+                        _startPosY -= step;
+                    }
+                    break;
+                case Direction.Down:
+                    if (_startPosY + step < _pictureHeight - etHeight)
+                    {
+                        _startPosY += step;
+                    }
+                    break;
+            }
+        }
+
+        public void DrawET(Graphics g)
+        {
+            Pen penMain = new Pen(MainColor);
+            Pen penDop = new Pen(DopColor);
+            Brush brMain = new SolidBrush(MainColor);
+            Brush brDop = new SolidBrush(DopColor);
+            Brush brFlag = new SolidBrush(FlagColor);
+            Brush brGray = new SolidBrush(Color.Gray);
+
+            g.FillRectangle(brMain, _startPosX + 3, _startPosY + 25, 50, 15);
+            g.DrawRectangle(penMain, _startPosX + 40, _startPosY + 7, 7, 25);
+
+            g.FillEllipse(brGray, _startPosX, _startPosY + 35, 55, 15);
+            g.FillEllipse(brDop, _startPosX + 2, _startPosY + 38, 10, 10);
+            g.FillEllipse(brDop, _startPosX + 15, _startPosY + 38, 10, 10);
+            g.FillEllipse(brDop, _startPosX + 28, _startPosY + 38, 10, 10);
+            g.FillEllipse(brDop, _startPosX + 41, _startPosY + 38, 10, 10);
+
+            g.DrawLine(penDop, _startPosX + 3, _startPosY + 30, _startPosX - 2, _startPosY + 30);
+            g.DrawLine(penDop, _startPosX + 3, _startPosY + 35, _startPosX - 2, _startPosY + 35);
+            g.DrawLine(penDop, _startPosX - 2, _startPosY + 20, _startPosX - 2, _startPosY + 45);
+            g.DrawLine(penDop, _startPosX - 2, _startPosY + 20, _startPosX - 5, _startPosY + 15);
+            g.DrawLine(penDop, _startPosX - 2, _startPosY + 45, _startPosX - 5, _startPosY + 50);
+
+            g.DrawLine(penDop, _startPosX + 50, _startPosY + 25, _startPosX + 60, _startPosY + 10);
+            g.DrawLine(penDop, _startPosX + 60, _startPosY + 10, _startPosX + 80, _startPosY + 25);
+            g.DrawLine(penDop, _startPosX + 80, _startPosY + 25, _startPosX + 80, _startPosY + 45);
+            g.DrawLine(penDop, _startPosX + 80, _startPosY + 30, _startPosX + 70, _startPosY + 30);
+            g.DrawLine(penDop, _startPosX + 80, _startPosY + 45, _startPosX + 65, _startPosY + 50);
+
+            if (Flag) {
+                g.DrawLine(penDop, _startPosX + 47, _startPosY + 10, _startPosX + 47, _startPosY);
+                g.FillRectangle(brFlag, _startPosX + 47, _startPosY, 20, 7);
+            }
+
+            if (FrontTube) {
+                g.FillRectangle(brMain, _startPosX + 8, _startPosY + 10, 5, 15);
+            }
+
+            if (BackTube) {
+                g.FillRectangle(brMain, _startPosX + 16, _startPosY + 10, 5, 15);
+            }
+        }
+    }
+}
---
+++ FormET.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Lab1
+{
+    partial class FormET
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxET = new System.Windows.Forms.PictureBox();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxET)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxET
+            // 
+            this.pictureBoxET.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxET.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxET.Name = "pictureBoxET";
+            this.pictureBoxET.Size = new System.Drawing.Size(878, 444);
+            this.pictureBoxET.TabIndex = 0;
+            this.pictureBoxET.TabStop = false;
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(19, 20);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(103, 35);
+            this.buttonCreate.TabIndex = 1;
+            this.buttonCreate.Text = "Create";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonLeft.Location = new System.Drawing.Point(746, 384);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(30, 30);
+            this.buttonLeft.TabIndex = 2;
+            this.buttonLeft.Text = "Left";
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRight.Location = new System.Drawing.Point(820, 384);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(30, 30);
+            this.buttonRight.TabIndex = 3;
+            this.buttonRight.Text = "Right";
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonUp.Location = new System.Drawing.Point(784, 351);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(30, 30);
+            this.buttonUp.TabIndex = 4;
+            this.buttonUp.Text = "Up";
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDown.Location = new System.Drawing.Point(784, 384);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(30, 30);
+            this.buttonDown.TabIndex = 5;
+            this.buttonDown.Text = "Down";
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(878, 444);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.pictureBoxET);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxET)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxET;
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonDown;
+    }
+}
+
